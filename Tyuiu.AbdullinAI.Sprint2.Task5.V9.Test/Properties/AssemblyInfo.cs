0b50d150fd982bd94eb9e99ad9552fd0514b0c5f
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-10-11 23:56:15 +0500

    Task5

---
+++ Tyuiu.AbdullinAI.Sprint2.Task5.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AbdullinAI.Sprint2.Task5.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AbdullinAI.Sprint2.Task5.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd5184fc-58f2-40a8-b2ef-61ba19c38b55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
