8b18a68255d1a8f35fb9f1277b343dd7b0a6daeb
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-10-12 20:43:31 +0500

    Task6

---
+++ Tyuiu.AbdullinAI.Sprint2.Task6.V6.Test/DataServiceTEst.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AbdullinAI.Sprint2.Task6.V6.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint2.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindCardNameAndValue()
+        {
+            DataService ds = new DataService();
+           
+            Assert.AreEqual("Шестёрка пики", ds.FindCardNameAndValue(1, 6));
+
+
+        }
+    }
+}
---
+++ Tyuiu.AbdullinAI.Sprint2.Task6.V6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AbdullinAI.Sprint2.Task6.V6.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint2.Task6.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Абудллин А. И.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Абдуллин А. И. | АСОиУБ-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Введите номер масти карты: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите номер достоинства карты: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            
+
+            string res = ds.FindCardNameAndValue(x,y);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
